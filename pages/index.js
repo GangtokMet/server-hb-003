0e01de2b42bdc3416a7387efa7e05a9d2abc3fab
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-15 17:29:05 +0530

    article added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
         <title>WEBApp Newz</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-      <h1>Welcome to NEXT app</h1>
+      
+      {articles.map((article) => (
+        <h3>{article.title}</h3>
+        // <p>{article.</p>
+      ))}
+
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
