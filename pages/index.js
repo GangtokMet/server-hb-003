c7f920118fd9b5e6a3d591f0bfc702f82aa482f7
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-15 16:52:59 +0530

    Layout 03

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WEBApp Newz</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
       <h1>Welcome to NEXT app</h1>
     </div>
   )
