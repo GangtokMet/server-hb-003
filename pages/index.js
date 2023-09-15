65f1151e5e9eee3cd0227e8fc27cebbc28fc5c17
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-15 18:20:48 +0530

    test01

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import ArticleList from '../components/ArticleList'
+// import ArticleList from '../components/ArticleList'
 
 export default function Home({ articles }) {
   console.log(articles)
