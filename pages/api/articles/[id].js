054636032ef291b1d4a2d141283f0164cb31f6b7
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-16 13:09:36 +0530

    saa1

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -2,10 +2,12 @@ import { articles } from '../../../data'
 
 export default function handler({ query: { id } }, res) {
     const filtered = articles.filter((article) => article.id === id)
-    console.log(filtered)
-    if (filtered.lenght > 0) {
-        res.status(200).json(filtered[0])
+
+    if (filtered.length > 0) {
+      res.status(200).json(filtered[0])
     } else {
-        res.status(404).json({ message: `Article with the id of ${id} is not found.`})
+      res
+        .status(404)
+        .json({ message: `Article with the id of ${id} is not found` })
     }
 }
