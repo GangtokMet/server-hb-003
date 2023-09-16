f0327768c208bf8db175b8ca043577008c7f934c
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-16 11:54:26 +0530

    router fixed

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,29 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const Article = () => {
-    const router = useRouter()
-    const { id } = router.query
+const Article = ({ article }) => {
+    // const router = useRouter()
+    // const { id } = router.query
 
     return (
-        <div>
-            This is an article {id}
-        </div>
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br/>
+            <Link href='/'>&larr; Go Back</Link>
+        </>
     )
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
 }
 
 export default Article
