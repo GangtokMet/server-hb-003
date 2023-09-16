02387cf338cd8b7fe3cb6a501cedb5f81391c839
GangtokMet <107405301+GangtokMet@users.noreply.github.com>
2023-09-16 11:24:47 +0530

    add router

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router'
+
 const article = () => {
+    const router = useRouter()
+    const { id } = router.query
+
     return (
         <div>
-            This is an article
+            This is an article { id }
         </div>
     )
 }
